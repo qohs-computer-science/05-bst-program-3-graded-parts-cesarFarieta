766442ea17d8e78a0598c060fe8810f9fd2ad3e5
cesarFarieta <cesarfarietaJr@gmail.com>
2025-02-20 15:16:28 +0000

    Still working not done (2/20)

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -2,5 +2,38 @@ import java.lang.Comparable;
 
 public class BST implements BSTInterface
 {
+    private TreeNode root;
+    private TreeNode subroot;
+
+
+    public BST(){
+        root=null;
+    }
+
+	public void add(Comparable newVal){
+        if(root=null){
+            root= new TreeNode();
+        }
+        else if(newVal<=root.getValue())
+	        addHelper(newVal,root.getLeft(),root)
+        else 
+	        addHelper(newVal,root.getRight(),root)
+    }
+
+    private void addHelper(Comparable newVal,TreeNode child,TreeNode parent)
+    {
+        if(child==null){
+		    if(val.comparTo(parent.getValue())<=0 )
+			    parent.setLeft(new TreeNode);
+		    else 
+			    parent.setRight(new TreeNode);
+        }
+        
+        else if  (child vs value)
+		    addHelper(left)
+	    else 
+		    addHelper(right)
+    }
+
 
 }
--- BSTProject/src/BSTTester.java
+++ BSTProject/src/BSTTester.java
@@ -10,5 +10,5 @@
  public class BSTTester {
    public static void main(String[] args) {
    
-	
+   }
  }
